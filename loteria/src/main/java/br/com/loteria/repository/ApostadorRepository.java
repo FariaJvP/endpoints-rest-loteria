867a6554298ad818a0dc8bd42ee994e9b144297b
João Vitor <jvfaria.paz@gmail.com>
2021-02-22 17:37:46 -0300

    resolvendo importacoes

--- loteria/src/main/java/br/com/loteria/repository/ApostadorRepository.java
+++ loteria/src/main/java/br/com/loteria/repository/ApostadorRepository.java
@@ -1,5 +1,6 @@
 package br.com.loteria.repository;
 
+import br.com.loteria.modelo.Apostador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

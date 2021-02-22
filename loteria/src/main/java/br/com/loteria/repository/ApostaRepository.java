87c1c124e6189e7318219e8cfe60e5b2ac9c2484
João Vitor <jvfaria.paz@gmail.com>
2021-02-22 16:24:20 -0300

    adicionando metodos para uso nos controllers

--- loteria/src/main/java/br/com/loteria/repository/ApostaRepository.java
+++ loteria/src/main/java/br/com/loteria/repository/ApostaRepository.java
@@ -1,9 +1,20 @@
 package br.com.loteria.repository;
 
 import br.com.loteria.modelo.Aposta;
+import br.com.loteria.modelo.Apostador;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ApostaRepository extends JpaRepository<Aposta, Long> {
+
+    Page<Aposta> findApostaByApostadorId(Apostador apostador, Pageable pageable);
+
+    @Query("SELECT a FROM Aposta a WHERE a.id=:id AND a.apostadorId=:apostador")
+    Optional<Aposta> FindApostaIdByApostador(Long id, Apostador apostador);
 }
--- loteria/src/main/java/br/com/loteria/seguranca/TokenService.java
+++ loteria/src/main/java/br/com/loteria/seguranca/TokenService.java
@@ -1,5 +1,6 @@
 package br.com.loteria.seguranca;
 
+import br.com.loteria.modelo.Apostador;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
